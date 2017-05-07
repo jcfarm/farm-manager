bfa998f4bff2de9c7bfd5feacfa39772f925cfcc
David <903205927@qq.com>
2017-05-07 13:20:53 +0800

    template layout and no data

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './containers/App';
+import NotFoundPage from './containers/NotFoundPage.js';
+import LoginPage from './containers/LoginPage';
+import FieldAdd from './containers/FieldAdd';
+import TablePage from './containers/Users';
+import Dashboard from './containers/DashboardPage';
+import Fields from './containers/Fields';
+
+export default (
+  <Route>
+    <Route path="login" component={LoginPage}/>
+    <Route path="/" component={App}>
+      <IndexRoute component={Dashboard}/>
+      <Route path="dashboard" component={Dashboard}/>
+      <Route path="fields/">
+        <IndexRoute component={Fields}/>
+        <Route path="add" component={FieldAdd}/>
+      </Route>
+      <Route path="users" component={TablePage}/>
+      <Route path="*" component={NotFoundPage}/>
+    </Route>
+  </Route>
+);
